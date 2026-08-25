b00a0530f50329a4e61e4b49f55a56ef4d01b9f9
Mayank Aylani <aylanimayank11@gmail.com>
2026-08-25 22:31:53 +0530

    A. Word Capitalization
    Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
    
    Note, that during capitalization all the letters except the first one remains unchanged.
    
    Input
    A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
    
    Output
    Output the given word after capitalization.
    
    Examples
    Input
    ApPLe
    Output
    ApPLe

---
+++ cf281A.cpp
@@ -0,0 +1,30 @@
+/*
+A. Word Capitalization
+
+Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+Input
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+
+Output
+Output the given word after capitalization.
+
+Examples
+Input
+ApPLe
+Output
+ApPLe
+*/
+#include<iostream>
+#include<cctype>
+using namespace std;
+int main()
+{
+string s;
+cin>>s;
+s[0]=toupper(s[0]);
+cout<<s;
+return 0;
+}
